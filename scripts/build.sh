a4270584916b0fda6b1b83cf1bae3f42a0388637
fwang <fwang@riverbed.com>
2018-09-12 19:05:26 -0400

    cleaned up the directory, convert to supply buildpack by removing unnecessary files

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,3 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
 GOOS=linux go build -ldflags="-s -w" -o bin/supply netcore_riverbed/supply/cli
-GOOS=linux go build -ldflags="-s -w" -o bin/finalize netcore_riverbed/finalize/cli
